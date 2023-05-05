c0993aee64d18ac628a5ee0628dc58ad07659973
Kesler <kesler.xiang@student.tdsb.on.ca>
2023-05-05 15:12:43 -0400

    e

--- assignment4/src/App.vue
+++ assignment4/src/App.vue
@@ -6,7 +6,7 @@ let information = ref(null);
 async function grabOption() {
   const id = movies.value;
   console.log(id);
-  information.value = axios.get(
+  information.value = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&append_to_response=videos`
   );
   let info = information.data;
@@ -15,46 +15,44 @@ async function grabOption() {
 </script>
 
 <template>
-  <html lang="en">
-    <body>
-      <div id="main" v-if="information">
-        <h1>Movies for movie night</h1>
-        <p>Based off of an objectively good opinon.</p>
-        <label for="movie">Choose a movie:</label>
-        <select id="movies">
-          <option value="635302">Demon Slayer: Mugen Train</option>
-          <option value="610150">Dragon Ball Super: Super Hero</option>
-          <option value="503314">Dragon Ball Super: Broly</option>
-          <option value="129">Spirited Away</option>
-          <option value="8392">My Neighbour Totoro</option>
-          <option value="900667">One Piece: Film Red</option>
-          <option value="315162">Puss in Boots: The Last Wish</option>
-          <option value="24428">The Avengers</option>
-          <option value="11836">The SpongeBob SquarePants Movie</option>
-          <option value="980078">Winnie the Pooh: Blood and Honey</option>
-        </select>
-        <button id="button" @click="grabOption()">Load</button>
-        <h1 id="title"></h1>
-        <h3 id="overview" class="clear"></h3>
-        <div id="grid">
-          <div id="storage">
-            <h4 id="overview"></h4>
-            <h4 id="original-language"></h4>
-            <h4 id="budget"></h4>
-            <h4 id="genre"></h4>
-            <h4 id="vote-average"></h4>
-            <h4 id="vote-count"></h4>
-            <h4 id="release-date"></h4>
-            <h4 id="popularity"></h4>
-            <h4 id="summary"></h4>
-            <h4 id="revenue"></h4>
-            <iframe id="video" allowfullscreen></iframe>
-          </div>
-          <div><img id="poster" src="" /><br /></div>
+  <body>
+    <div id="main" v-if="information">
+      <h1>Movies for movie night</h1>
+      <p>Based off of an objectively good opinon.</p>
+      <label for="movie">Choose a movie:</label>
+      <select id="movies">
+        <option value="635302">Demon Slayer: Mugen Train</option>
+        <option value="610150">Dragon Ball Super: Super Hero</option>
+        <option value="503314">Dragon Ball Super: Broly</option>
+        <option value="129">Spirited Away</option>
+        <option value="8392">My Neighbour Totoro</option>
+        <option value="900667">One Piece: Film Red</option>
+        <option value="315162">Puss in Boots: The Last Wish</option>
+        <option value="24428">The Avengers</option>
+        <option value="11836">The SpongeBob SquarePants Movie</option>
+        <option value="980078">Winnie the Pooh: Blood and Honey</option>
+      </select>
+      <button id="button" @click="grabOption()">Load</button>
+      <h1 id="title"></h1>
+      <h3 id="overview" class="clear"></h3>
+      <div id="grid" v-if="information">
+        <div id="storage">
+          <h4 id="overview">{{ information.overview }}</h4>
+          <h4 id="original-language"></h4>
+          <h4 id="budget"></h4>
+          <h4 id="genre"></h4>
+          <h4 id="vote-average"></h4>
+          <h4 id="vote-count"></h4>
+          <h4 id="release-date"></h4>
+          <h4 id="popularity"></h4>
+          <h4 id="summary"></h4>
+          <h4 id="revenue"></h4>
+          <iframe id="video" allowfullscreen></iframe>
         </div>
+        <div><img id="poster" src="" /><br /></div>
       </div>
-    </body>
-  </html>
+    </div>
+  </body>
 </template>
 
 <style scoped>
@@ -67,11 +65,15 @@ body {
   text-align: center;
   color: rgb(229, 230, 170);
   font-family: "limelight", "Space Mono", monospace;
+  width: 100vh;
 }
 #poster {
   height: 50rem;
   width: 35rem;
   border-width: 0cm;
+}
+#Load {
+  background-color: blue;
 }
 #grid {
   display: grid;
